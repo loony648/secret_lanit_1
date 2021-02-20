e29a8e546a3ad75d68426f4bd71ed00161339421
Nikita Anisimov <anisimov0324@gmail.com>
2021-02-20 13:01:14 +0300

    20.02.20

--- script.py
+++ script.py
@@ -2,7 +2,6 @@ import requests
 import time
 from datetime import datetime, timezone
 
-bitfinexURL = 'https://api-pub.bitfinex.com/v2'
 influxURL = 'http://localhost:8086'
 dbName = 'bitfinexdb'
 
@@ -36,7 +35,7 @@ class InfluxDBClient:
             InfluxDBClient.ping
             
             Params:
-            @payload - data to send
+            None
 
             Returns:
             Check server status
@@ -75,54 +74,57 @@ class InfluxDBClient:
         print('Sending write request to ' + f'{influxWriteURL}')
         return req
 
-    def ParseToInlineQuery(self, bitfinexAnswer):
+    def ParseToInlineQuery(self, bitfinexLists):
         """
             InfluxDBClient.ParseToInlineQuery
             
             Params:
-            @bitfinexAnswer - bitfinex HTTP GET return
+            @bitfinexLists - List with data
 
             Returns:
             Parsed Bitfinex answer into line query for InfluxDB
         """
         InlineQuery = ''
-        #timestamp = (datetime.now(timezone.utc).astimezone()).isoformat()
-        for bitfinexLists in bitfinexAnswer:
-            if(len(bitfinexLists) == len(tradingPairsList)):
-                symbol, bid, bidSize, ask, askSize, dailyChange, dailyChangeRelative, lastPrice, volume, high, low = bitfinexLists
+        for bitfinexList in bitfinexLists:
+            if(len(bitfinexList) == len(tradingPairsList)):
+                symbol, bid, bidSize, ask, askSize, dailyChange, dailyChangeRelative, lastPrice, volume, high, low = bitfinexList
                 InlineQuery += f'{self.dbName},pair={symbol} bid={bid},bid_size={bidSize},ask={ask},ask_size={askSize},daily_change={dailyChange},daily_change_rel={dailyChangeRelative},last_price={lastPrice},volume={volume},high={high},low={low}\n'
         return InlineQuery
 
-def bitfinexGetAllTickers():
-    """
-        bitfinexGetAllTickers
+class BitFinexV2:
+    def __init__(self):
+        self.serverURL = 'https://api-pub.bitfinex.com/v2'
         
-        Params:
-        None
+    def GetAllTickers(self):
+        """
+            bitfinexGetAllTickers
+            
+            Params:
+            None
 
-        Returns:
-        Returns all bitfinex tickers by HTTP GET
-    """
-    bitfinexGetURL = bitfinexURL + '/tickers?symbols=ALL'
-    req = requests.get(bitfinexGetURL)
-    print('Sending get request to ' + f'{bitfinexGetURL}')
-    return req
+            Returns:
+            Returns all bitfinex tickers by HTTP GET
+        """
+        bitfinexGetURL = self.serverURL + '/tickers?symbols=ALL'
+        req = requests.get(bitfinexGetURL)
+        print('Sending get request to ' + f'{bitfinexGetURL}')
+        return req
 
 
 if __name__ == '__main__':
     influxClient = InfluxDBClient(dbName, influxURL)
+    BitFinexClient = BitFinexV2()
+
     while(True):
         pingReq = influxClient.ping()
-        if(pingReq.status_code != 204):
-            SystemExit('Some problems with DB. Check health of your influxDB server. Exit..')
-
-        bitfinexTickersList = bitfinexGetAllTickers()
-        bitfinexAnswer = influxClient.ParseToInlineQuery(bitfinexTickersList.json())
-        influxReturn = influxClient.httpWrite(bitfinexAnswer)
+        bitfinexTickersList = BitFinexClient.GetAllTickers()
 
         if(bitfinexTickersList.status_code != 200):
             SystemExit('Check connection with bitfinex api. Exit..')
 
+        bitfinexAnswer = influxClient.ParseToInlineQuery(bitfinexTickersList.json())
+
+        influxReturn = influxClient.httpWrite(bitfinexAnswer)
         if(influxReturn.status_code == 404):
             influxCreateReturn = influxClient.httpQuery('CREATE DATABASE ' + dbName)
             print('InfluxDB didnt found DB. Trying to create db.')
